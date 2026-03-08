7ff00b02748522530cd1fbd0b0e75eae02040540
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2026-03-07 20:25:32 -0600

    create home section

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,8 +4,27 @@ export default async function HomePage() {
   const t = await getTranslations("HomePage");
 
   return (
-    <div className="w-full h-screen color-transition flex items-center justify-center bg-primary-light-50 dark:bg-primary-dark-500">
-      <h1>{t("title")}</h1>
+    <div className="w-full h-screen color-transition bg-app">
+      <section id="home" className="section">
+        <div className="w-full h-full flex flex-col items-center justify-center space-y-8">
+          <div className="badge color-transition">
+            <span className="status-dot">
+              <span className="status-dot-ping" />
+              <span className="status-dot-core" />
+            </span>
+            <p>Disponible para nuevas oportunidades</p>
+          </div>
+          <p className="text-heading">
+            Anthony
+            <br />
+            Martinez
+          </p>
+          <p className="text-subheading text-center">
+            Ingeniero en Desarrollo y Gestión de Software en plataformas
+            móviles, web y de escritorio con enfoque en el área UX/UI.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,20 +4,19 @@ import { getTranslations } from "next-intl/server";
 
 export default async function Navbar() {
   const t = await getTranslations("Navigator");
-  const t2 = await getTranslations("Language");
   const t4 = await getTranslations();
 
   return (
-    <nav className="sticky top-0 w-full bg-primary-light-300 dark:bg-primary-dark-400 shadow-md z-50 color-transition">
-      <div className="w-full mx-auto px-4 py-4 flex justify-between items-center">
+    <nav className="sticky top-0 w-full bg-nav border-base z-50 color-transition">
+      <div className="w-full mx-auto py-5 flex justify-between items-center">
         <div className="flex w-1/5 justify-center">
           <h1 className="text-xl font-bold">{t4("title")}</h1>
         </div>
 
         <ul className="flex gap-x-6 w-3/5 justify-center">
-          <li>{t("homeTitle")}</li>
           <li>{t("aboutTitle")}</li>
           <li>{t("projectsTitle")}</li>
+          <li>{t("experienceTitle")}</li>
           <li>{t("contactTitle")}</li>
         </ul>
 
