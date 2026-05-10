dc601c16a0b7d109d85972fbdc00a50793ff78d5
Anthony Martinez Arellano <116910131+AnthonyMA5@users.noreply.github.com>
2026-05-10 15:13:59 -0600

    feat: implement responsive Navbar component and define global Tailwind CSS theme and styles

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -12,7 +12,7 @@ export default function Navbar() {
 
   return (
     <nav className="fixed top-0 left-0 right-0 w-full bg-light-50/50 dark:bg-dark-400/70 color-transition backdrop-blur-lg">
-      <div className="w-full mx-auto px-6 py-5 flex justify-between items-center">
+      <div className="w-full mx-auto px-6 py-4 flex justify-between items-center">
         <div className="flex md:w-1/5 justify-start md:justify-center">
           <h1 className="text-xl font-bold">
             <a href="#">{t4("title")}</a>
